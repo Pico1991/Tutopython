1c3ad4042703ae41a0d4d2242831ca481163fb29
Pico1991 <152601545+Pico1991@users.noreply.github.com>
2023-12-06 10:12:51 +0100

    Fin de la vidéo fichier

---
+++ Tuto_Graven/Legacy.py
@@ -0,0 +1,14 @@
+from model.Batiment import *
+
+# 4 immeubles
+immeuble1 = Immeuble("26 rue de la Gravenade", 3, 3)
+immeuble2 = Immeuble("28 rue de la Grevande", 5, 6)
+immeuble3 = Immeuble("53 rue elios mitterand", 2, 2)
+immeuble5 = Immeuble("120 rue pleiades", 8, 4)
+
+# 2 supermarché
+supermarche1 = Supermarche("27 rue de la Gravenade", 1, 12)
+supermarche2 = Supermarche("119 rue pleiades", 4, 25)
+
+# 1 banque
+banque = Banque("53 rue elios mitterand", 25,50,"GravenBanque")
